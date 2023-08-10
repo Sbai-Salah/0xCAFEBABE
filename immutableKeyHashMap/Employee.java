a599a2b647dc2e464380ed9f5620d7d51da97730
Sbai-Salah <sbaiusalah@gmail.com>
2023-08-10 11:04:00 +0100

    immutable key hashmap

---
+++ immutableKeyHashMap/Employee.java
@@ -0,0 +1,60 @@
+package immutableKeyHashMap;
+
+public class Employee {
+    public String name;
+    public int age;
+
+    public Employee(String na, int ag){
+        this.name = na;
+        this.age = ag;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setName(String na){
+        this.name = na;
+    }
+
+    public void setAge(int ag){
+        this.age = ag;
+    }
+
+    @Override
+    public int hashCode(){
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + age;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj){
+        if(this == obj) 
+            return true;
+        if(obj == null) 
+            return false;
+        if(getClass() != obj.getClass()) 
+            return false;
+
+        Employee other = (Employee) obj;
+        if(age != other.age)
+            return false;
+        
+        if(name == null){
+            if(other.name != null){
+                return false;
+            }
+        }else if(!name.equals(other.name)){
+            return false;
+        }
+
+        return true;
+    }
+
+}
---
+++ immutableKeyHashMap/TestMap.java
@@ -0,0 +1,31 @@
+package immutableKeyHashMap;
+
+import java.util.HashMap;
+import java.util.Map;
+/*
+ * @author sbai-salah
+*/
+public class TestMap {
+
+    public static void main(String[] args) {
+
+        Map<Employee, String> empMap = new HashMap<Employee, String>();
+        System.out.println("--- DECLARING SIMPLE OBJECT e1 ---");
+        Employee e1 = new Employee("Salah", 2023);
+        empMap.put(e1, "ENGINEER");
+        System.out.println(empMap.get(e1)); // will display ENGINEER
+        // now let us mutate another object and see what will happen
+        Employee e2 = new Employee("Sbai", 111);
+        empMap.put(e2, "SCIENTISTS");
+        System.out.println("--- BEFORE MUTATING THE OBJECT e2 ---");
+        System.out.println(empMap.get(e2)); 
+        e2.setAge(150);
+        System.out.println("--- AFTER MUTATING THE OBJECT e2.age ---");
+        System.out.println(empMap.get(e2)); 
+        System.out.println("\nthe hashCode function depends on the age\n when changing the age or even the name we will get another bucket index with same object : memory lose\n ==> THAT'S WHY HASHMAP KEY SHOULD BE IMMUTABLE");
+
+
+
+    }
+    
+}
